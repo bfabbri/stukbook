23cf385250a2fbdc3c1f97e0413032ea76d1470c
Bruno Fabbri <fabbri07@gmail.com>
2015-07-06 14:04:02 -0300

    Addd posts controller, views and routes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  include PublicAtivity::Model
+  include PublicActivity::Model
   belongs_to :user
   validates_presence_of :user_id
   validates_presence_of :content
